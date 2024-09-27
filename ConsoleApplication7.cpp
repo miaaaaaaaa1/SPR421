7562167c77856fdad70ceaba97782cad0fb4b1ee
miaaaaaaaa1 <solomia1pochaievets@gmail.com>
2024-09-27 17:25:14 +0300

    Add files via upload

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <cstdlib> 
+#include <ctime>    
+using namespace std;
+
+int main() {
+//завдання 1
+    srand(static_cast<unsigned int>(time(0))); 
+
+    const int SIZE = 10; 
+    int arr[SIZE];
+
+    for (int i = 0; i < SIZE; ++i) {
+        arr[i] = rand() % 100;
+    }
+
+    cout << "Array: ";
+    for (int i = 0; i < SIZE; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int min = arr[0];
+    int max = arr[0];
+
+    for (int i = 1; i < SIZE; ++i) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    cout << "Minimum element: " << min << endl;
+    cout << "Maximum element: " << max << endl;
+
+    //завдання 2
+
+    const int MONTHS = 12;
+    double profit[MONTHS];
+    int startMonth, endMonth;
+
+    cout << "Enter the profit of the company for each month (12 months):" << endl;
+    for (int i = 0; i < MONTHS; ++i) {
+        cout << "Month" << i + 1 << ": ";
+        cin >> profit[i];
+    }
+
+    cout << "Enter a range of months: " << endl;
+    cin >> startMonth >> endMonth;
+
+    if (startMonth < 1 || endMonth > 12 || startMonth > endMonth) {
+        cout << "Incorrect range!" << endl;
+        return 1;
+    }
+
+    double minProfit = profit[startMonth - 1];
+    double maxProfit = profit[startMonth - 1];
+    int minMonth = startMonth;
+    int maxMonth = startMonth;
+
+    for (int i = startMonth - 1; i < endMonth; ++i) {
+        if (profit[i] < minProfit) {
+            minProfit = profit[i];
+            minMonth = i + 1;
+        }
+        if (profit[i] > maxProfit) {
+            maxProfit = profit[i];
+            maxMonth = i + 1;
+        }
+    }
+
+    cout << "Maximum profit per month " << maxMonth << ": " << maxProfit << endl;
+    cout << "Minimum profit per month " << minMonth << ": " << minProfit << endl;
+}
---
+++ ConsoleApplication8.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+   //завдання 1
+    int rows, cols, startValue;
+
+    cout << "Enter the number of strings of the array: ";
+    cin >> rows;
+    cout << "Enter the number of array columns: ";
+    cin >> cols;
+    cout << "Enter the starting number: ";
+    cin >> startValue;
+
+    int arr[rows][cols];
+
+    arr[0][0] = startValue;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (i == 0 && j == 0) {
+                continue;
+            }
+
+            if (j == 0) {             
+                arr[i][j] = arr[i - 1][cols - 1] * 2;
+            }
+            else {
+                arr[i][j] = arr[i][j - 1] * 2;
+            }
+        }
+    }
+
+    cout << "Created array:" << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    //завдання 2
+
+    int rows, cols, startValue;
+
+    cout << "Enter the number of strings of the array: ";
+    cin >> rows;
+    cout << "Enter the number of array columns:";
+    cin >> cols;
+    cout << "Enter the starting number: ";
+    cin >> startValue;
+
+    int arr[rows][cols];
+
+    int currentValue = startValue;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            arr[i][j] = currentValue; 
+            currentValue++;         
+        }
+    }
+
+    cout << "Created array:" << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << arr[i][j] << " "; 
+        }
+        cout << endl;
+    }
+}
